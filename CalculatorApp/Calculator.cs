aa18c8e9667e34117fc4474dd209cb6168ff79e8
whtduck1 <myszormateusz02@gmail.com>
2026-06-05 13:02:38 +0200

    copying files

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorApp;
+
+public class Calculator
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
